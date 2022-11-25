1ecf9506d852b1bec2c0a6f3343cda43a1cfea8f
ElgarMokaya <elgarmokaya01@gmail.com>
2022-11-25 04:49:03 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-portfolio';
+  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`;
+  desc='Frontend Developer(Angular) / Backend Developer(Django)'
+  course ='Bachelor of Science in Computer science'
+
+  images: Image[]  =[
+    {label:"ANGULAR",src:"../assets/pictures/angular.png"},
+    {label:"CSS3",src:"../assets/pictures/css31.png"},
+    {label:"HTML5",src:"../assets/pictures/html.png"},
+    {label:"JS",src:"../assets/pictures/javas.png"},
+    {label:"NODEJS",src:"../assets/pictures/nodejs3.png"},
+     {label:"PYTHON",src:"../assets/pictures/pythonlogo.png"},
+     {label:"DJANGO",src:"../assets/pictures/django34.png"}
+  ]
+  
+}
+class Image {
+  label!: string;
+  src !:string;
 }
